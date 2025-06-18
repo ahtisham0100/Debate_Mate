72acd8e72091b4e125d24e5bb408f2dc685a2f6e
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2025-06-18 17:57:53 +0500

    first push

---
+++ js/landingPage.js
@@ -0,0 +1,12 @@
+const primaryButton = document.querySelector(".primary_btn");
+
+primaryButton.addEventListener("click", function() {
+primaryButton.classList.add("animate_flash"); 
+console.log("Button clicked! Animation started.");
+primaryButton.classList.remove("animate__fadeInLeft")
+setTimeout(() => {
+    primaryButton.classList.remove("animate_flash");
+}, 2000);
+
+
+})
